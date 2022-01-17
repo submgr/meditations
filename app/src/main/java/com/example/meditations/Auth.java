59cc9100f5fddfbb9d50493f828a2289d65440e2
Aram Virabyan <googlaram200@gmail.com>
2022-01-17 14:48:36 +0300

    updated color

--- app/src/main/java/com/example/meditations/Auth.java
+++ app/src/main/java/com/example/meditations/Auth.java
@@ -5,9 +5,16 @@ import static android.content.ContentValues.TAG;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.IntentSender;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.identity.BeginSignInRequest;
 import com.google.android.gms.auth.api.identity.BeginSignInResult;
@@ -25,5 +32,19 @@ public class Auth extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
         getSupportActionBar().hide();
+
+        WebView webView = findViewById(R.id.auth_wv);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new AuthInterface(this), "Android");
+        webView.loadUrl("file:///android_asset/wv/auth.html");
+    }
+
+    public void secretbtn(View view){
+        startActivity(new Intent(Auth.this, HomeScreen.class));
+        //SharedPreferences preferences = getSharedPreferences("app_data", 0);
+        //SharedPreferences.Editor editor = preferences.edit();
+        //editor.putString("usage_state", "began");
+        //editor.apply();
     }
 }
